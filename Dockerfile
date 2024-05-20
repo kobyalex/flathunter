109993eec15ded9e1da03a0b430da126f4498908
Oleksii Koby <kobyalex@gmail.com>
2024-05-20 02:49:36 +0200

    ci: fixed image Docker dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,44 @@
-FROM python:3.11
+FROM arm32v7/python:3.11-slim-bullseye
 
+# Setting environment variables
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
-ARG PIP_NO_CACHE_DIR=1
 
-# Install Chromium
-RUN apt-get -y update
-RUN apt-get install -y chromium
+# Use a single RUN command for apt-get to reduce the number of layers
+RUN apt-get update && apt-get install -y \
+    chromium \
+    git \
+    build-essential \
+    pkg-config \
+    zlib1g-dev \
+    curl \
+    libssl-dev \
+    libffi-dev \
+    libxml2-dev \
+    libxslt1-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
 
-# Upgrade pip, install pipenv
-RUN pip install --upgrade pip
-RUN pip install pipenv
+# Install Rust using the official installer script
+RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
+# Add Rust to PATH (this might vary depending on the base image)
+ENV PATH="/root/.cargo/bin:${PATH}"
+
+# Confirm installation by printing Rust version
+RUN rustc --version
+
+# Upgrade pip and install pipenv in one RUN command
+RUN pip install --upgrade pip && pip install pipenv
+
+# Set the working directory
 WORKDIR /usr/src/app
 
-# Copy files that list dependencies
-COPY Pipfile.lock Pipfile ./
+# Copy Pipfile and Pipfile.lock before installing dependencies
+COPY Pipfile Pipfile.lock ./
 
 # Generate requirements.txt and install dependencies from there
-RUN pipenv requirements > requirements.txt
-RUN pip install -r requirements.txt
+RUN pipenv requirements > requirements.txt && pip install -r requirements.txt
 
-# Copy all other files, including source files
+# Copy the rest of the application code
 COPY . .
--- flathunter/crawler/wggesucht.py
+++ flathunter/crawler/wggesucht.py
@@ -92,60 +92,6 @@ def is_verified_company(row: Tag) -> bool:
         return True
     return False
 
-# pylint: disable=too-many-return-statements
-def parse_expose_element_to_details(row: Tag, crawler: str) -> Optional[Dict]:
-    """Parse an Expose soup element to an Expose details dictionary"""
-    title_row = row.find('h3', {"class": "truncate_title"})
-    if title_row is None or not isinstance(title_row, Tag):
-        logger.warning("No title found - skipping")
-        return None
-    if is_verified_company(row):
-        logger.warning("Advert found - skipping")
-        return None
-    title = get_title(title_row)
-    url = get_url(title_row)
-    if url is None:
-        logger.warning("No expose URL found - skipping")
-        return None
-    image = get_image_url(row)
-    rooms = get_rooms(row)
-    numbers_row = row.find("div", {"class": "middle"})
-    if not isinstance(numbers_row, Tag):
-        logger.warning("No numbers row found - skipping")
-        return None
-    price = get_price(numbers_row)
-    dates = get_dates(numbers_row)
-    if len(dates) == 0:
-        logger.warning("No dates found - skipping")
-        return None
-    size = get_size(numbers_row)
-    if len(size) == 0:
-        logger.warning("No size found - skipping")
-        return None
-
-    if len(dates) == 2:
-        title = f"{title} vom {dates[0]} bis {dates[1]}"
-    else:
-        title = f"{title} ab dem {dates[0]}"
-
-    details = {
-        'id': int(url.split('.')[-2]),
-        'image': image,
-        'url': url,
-        'title': title,
-        'price': price,
-        'size': size[0],
-        'rooms': rooms,
-        'address': url,
-        'crawler': crawler
-    }
-    if len(dates) == 2:
-        details['from'] = dates[0]
-        details['to'] = dates[1]
-    elif len(dates) == 1:
-        details['from'] = dates[0]
-    return details
-
 
 def liste_attribute_filter(element: Union[Tag, str]) -> bool:
     """Return true for elements whose 'id' attribute starts with 'liste-' 
@@ -180,7 +126,7 @@ class WgGesucht(Crawler):
             if isinstance(e, Tag) and e.has_attr('class') and not 'display-none' in e['class']
         ]
         for row in existing_findings:
-            details = parse_expose_element_to_details(row, self.get_name())
+            details = self.parse_expose_element_to_details(row, self.get_name())
             if details is None:
                 continue
             entries.append(details)
@@ -192,15 +138,15 @@ class WgGesucht(Crawler):
     def load_address(self, url) -> Optional[str]:
         """Extract address from expose itself"""
         response = self.get_soup_from_url(url)
-        address_div = response.find('div', {"class": "col-sm-4 mb10"})
-        if not isinstance(address_div, Tag):
-            logger.debug("No address in response for URL: %s", url)
-            return None
-        a_element = address_div.find("a", {"href": "#mapContainer"})
+        a_element = response.find("a", {"href": "#mapContainer"})
         if not isinstance(a_element, Tag):
             logger.debug("No address in response for URL: %s", url)
             return None
-        return ' '.join(a_element.text.strip().split())
+        a_span = a_element.find("span", {"class": "section_panel_detail"})
+        if not isinstance(a_span, Tag):
+            logger.debug("No address in response for URL: %s", url)
+            return None
+        return ' '.join(a_span.text.strip().split())
 
     def get_soup_from_url(
             self,
@@ -235,3 +181,59 @@ class WgGesucht(Crawler):
                     driver, checkbox, afterlogin_string or "")
             return BeautifulSoup(driver.page_source, 'lxml')
         return BeautifulSoup(resp.content, 'lxml')
+    
+    # pylint: disable=too-many-return-statements
+    def parse_expose_element_to_details(self, row: Tag, crawler: str) -> Optional[Dict]:
+        """Parse an Expose soup element to an Expose details dictionary"""
+        title_row = row.find('h3', {"class": "truncate_title"})
+        if title_row is None or not isinstance(title_row, Tag):
+            logger.warning("No title found - skipping")
+            return None
+        if is_verified_company(row):
+            logger.warning("Advert found - skipping")
+            return None
+        title = get_title(title_row)
+        url = get_url(title_row)
+        if url is None:
+            logger.warning("No expose URL found - skipping")
+            return None
+        image = get_image_url(row)
+        rooms = get_rooms(row)
+        numbers_row = row.find("div", {"class": "middle"})
+        if not isinstance(numbers_row, Tag):
+            logger.warning("No numbers row found - skipping")
+            return None
+        price = get_price(numbers_row)
+        dates = get_dates(numbers_row)
+        if len(dates) == 0:
+            logger.warning("No dates found - skipping")
+            return None
+        size = get_size(numbers_row)
+        if len(size) == 0:
+            logger.warning("No size found - skipping")
+            return None
+
+        if len(dates) == 2:
+            title = f"{title} vom {dates[0]} bis {dates[1]}"
+        else:
+            title = f"{title} ab dem {dates[0]}"
+
+        address = self.load_address(url)
+        
+        details = {
+            'id': int(url.split('.')[-2]),
+            'image': image,
+            'url': url,
+            'title': title,
+            'price': price,
+            'size': size[0],
+            'rooms': rooms,
+            'address': address,
+            'crawler': crawler
+        }
+        if len(dates) == 2:
+            details['from'] = dates[0]
+            details['to'] = dates[1]
+        elif len(dates) == 1:
+            details['from'] = dates[0]
+        return details
