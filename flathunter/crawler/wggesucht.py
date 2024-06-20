9dac0984c9bfd2e076e56aa09a2267acb4a81a28
Oleksii Kobylianskyi <kobyalex@gmail.com>
2024-06-20 23:28:34 +0200

    fix: fixed wg-gesucht crawler

--- flathunter/crawler/wggesucht.py
+++ flathunter/crawler/wggesucht.py
@@ -7,7 +7,6 @@ from bs4 import BeautifulSoup, Tag
 
 from flathunter.logging import logger
 from flathunter.abstract_crawler import Crawler
-from flathunter.string_utils import remove_prefix
 
 
 def get_title(title_row: Tag) -> str:
@@ -22,7 +21,7 @@ def get_url(title_row: Tag) -> Optional[str]:
             or not a_element.has_attr('href') \
             or not isinstance(a_element.attrs['href'], str):
         return None
-    return 'https://www.wg-gesucht.de/' + remove_prefix(a_element.attrs['href'], "/")
+    return 'https://www.wg-gesucht.de/' + a_element.attrs['href'].removeprefix("/")
 
 
 def extract_href_style(row: Tag) -> Optional[str]:
@@ -93,68 +92,18 @@ def is_verified_company(row: Tag) -> bool:
         return True
     return False
 
-# pylint: disable=too-many-return-statements
-def parse_expose_element_to_details(row: Tag, crawler: str) -> Optional[Dict]:
-    """Parse an Expose soup element to an Expose details dictionary"""
-    title_row = row.find('h3', {"class": "truncate_title"})
-    if title_row is None or not isinstance(title_row, Tag):
-        logger.warning("No title found - skipping")
-        return None
-    if is_verified_company(row):
-        logger.warning("Advert found - skipping")
-        return None
-    title = get_title(title_row)
-    url = get_url(title_row)
-    if url is None:
-        logger.warning("No expose URL found - skipping")
-        return None
-    image = get_image_url(row)
-    rooms = get_rooms(row)
-    numbers_row = row.find("div", {"class": "middle"})
-    if not isinstance(numbers_row, Tag):
-        logger.warning("No numbers row found - skipping")
-        return None
-    price = get_price(numbers_row)
-    dates = get_dates(numbers_row)
-    if len(dates) == 0:
-        logger.warning("No dates found - skipping")
-        return None
-    size = get_size(numbers_row)
-    if len(size) == 0:
-        logger.warning("No size found - skipping")
-        return None
-
-    if len(dates) == 2:
-        title = f"{title} vom {dates[0]} bis {dates[1]}"
-    else:
-        title = f"{title} ab dem {dates[0]}"
-
-    details = {
-        'id': int(url.split('.')[-2]),
-        'image': image,
-        'url': url,
-        'title': title,
-        'price': price,
-        'size': size[0],
-        'rooms': rooms,
-        'address': url,
-        'crawler': crawler
-    }
-    if len(dates) == 2:
-        details['from'] = dates[0]
-        details['to'] = dates[1]
-    elif len(dates) == 1:
-        details['from'] = dates[0]
-    return details
-
 
 def liste_attribute_filter(element: Union[Tag, str]) -> bool:
-    """Return true for elements whose 'id' attribute starts with 'liste-'"""
+    """Return true for elements whose 'id' attribute starts with 'liste-' 
+    and are not contained in the 'premium_user_extra_list' container"""
     if not isinstance(element, Tag):
         return False
-    if "id" not in element.attrs:
+    if not element.attrs or "id" not in element.attrs:
         return False
-    return element.attrs["id"].startswith('liste-')
+    if not element.parent or not element.parent.attrs or "class" not in element.parent.attrs:
+        return False
+    return element.attrs["id"].startswith('liste-') and \
+        'premium_user_extra_list' not in element.parent.attrs["class"]
 
 
 class WgGesucht(Crawler):
@@ -176,9 +125,8 @@ class WgGesucht(Crawler):
             e for e in findings
             if isinstance(e, Tag) and e.has_attr('class') and not 'display-none' in e['class']
         ]
-
         for row in existing_findings:
-            details = parse_expose_element_to_details(row, self.get_name())
+            details = self.parse_expose_element_to_details(row, self.get_name())
             if details is None:
                 continue
             entries.append(details)
@@ -190,15 +138,15 @@ class WgGesucht(Crawler):
     def load_address(self, url) -> Optional[str]:
         """Extract address from expose itself"""
         response = self.get_soup_from_url(url)
-        address_div = response.find('div', {"class": "col-sm-4 mb10"})
-        if not isinstance(address_div, Tag):
-            logger.debug("No address in response for URL: %s", url)
-            return None
-        a_element = address_div.find("a", {"href": "#mapContainer"})
+        a_element = response.find("a", {"href": "#mapContainer"})
         if not isinstance(a_element, Tag):
             logger.debug("No address in response for URL: %s", url)
             return None
-        return ' '.join(a_element.text.strip().split())
+        a_span = a_element.find("span", {"class": "section_panel_detail"})
+        if not isinstance(a_span, Tag):
+            logger.debug("No address in response for URL: %s", url)
+            return None
+        return ' '.join(a_span.text.strip().split())
 
     def get_soup_from_url(
             self,
@@ -231,5 +179,61 @@ class WgGesucht(Crawler):
             elif re.search("g-recaptcha", driver.page_source):
                 self.resolve_recaptcha(
                     driver, checkbox, afterlogin_string or "")
-            return BeautifulSoup(driver.page_source, 'html.parser')
-        return BeautifulSoup(resp.content, 'html.parser')
+            return BeautifulSoup(driver.page_source, 'lxml')
+        return BeautifulSoup(resp.content, 'lxml')
+    
+    # pylint: disable=too-many-return-statements
+    def parse_expose_element_to_details(self, row: Tag, crawler: str) -> Optional[Dict]:
+        """Parse an Expose soup element to an Expose details dictionary"""
+        title_row = row.find('h3', {"class": "truncate_title"})
+        if title_row is None or not isinstance(title_row, Tag):
+            logger.warning("No title found - skipping")
+            return None
+        if is_verified_company(row):
+            logger.warning("Advert found - skipping")
+            return None
+        title = get_title(title_row)
+        url = get_url(title_row)
+        if url is None:
+            logger.warning("No expose URL found - skipping")
+            return None
+        image = get_image_url(row)
+        rooms = get_rooms(row)
+        numbers_row = row.find("div", {"class": "middle"})
+        if not isinstance(numbers_row, Tag):
+            logger.warning("No numbers row found - skipping")
+            return None
+        price = get_price(numbers_row)
+        dates = get_dates(numbers_row)
+        if len(dates) == 0:
+            logger.warning("No dates found - skipping")
+            return None
+        size = get_size(numbers_row)
+        if len(size) == 0:
+            logger.warning("No size found - skipping")
+            return None
+
+        if len(dates) == 2:
+            title = f"{title} vom {dates[0]} bis {dates[1]}"
+        else:
+            title = f"{title} ab dem {dates[0]}"
+
+        address = self.load_address(url)
+        
+        details = {
+            'id': int(url.split('.')[-2]),
+            'image': image,
+            'url': url,
+            'title': title,
+            'price': price,
+            'size': size[0],
+            'rooms': rooms,
+            'address': address,
+            'crawler': crawler
+        }
+        if len(dates) == 2:
+            details['from'] = dates[0]
+            details['to'] = dates[1]
+        elif len(dates) == 1:
+            details['from'] = dates[0]
+        return details
